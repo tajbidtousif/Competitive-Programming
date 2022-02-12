e8bf979ad08a33825e326e1ceb8c64884e417b04
Shah Tajbid Tousif <86789646+tajbidtousif@users.noreply.github.com>
2022-02-12 18:05:30 +0600

    part 2

--- SecondLarge.cpp
+++ SecondLarge.cpp
@@ -5,26 +5,37 @@ using namespace std;
 
 int main()
 {
-    int first = 0, second = -1;
-    int array[5] = {30, 2, 5, 50, 3};
+    int first = 0, second = -1, uncmn[105];
 
-    for(int i=0; i<5; i++)
+    int array[5] = {5, 5, 5, 5, 5};
+
+    int n = sizeof(array) / sizeof(array[0]);
+    sort(array, array+n);
+    
+    int k=0;
+
+    for(int i=0; i<n; i++)
     {
-        if(array[i]>first)
+        if(array[i] != array[i+1])
         {
-            second = first;
-            first = array[i];
-        }
-        else if(array[i] != first)
-        {
-            if(array[i] > second)
-            {
-                second = array[i];
-            }
+            uncmn[k] = array[i];
+            k++;        
         }
     }
-    cout << "First largest is : " << first << endl;
-    cout << "Second largest is : " << second << endl;
+
+
+
+    cout << "First largest number is : " << uncmn[k-1] << endl; 
+
+    if(k==1)
+    {
+        cout << "No second largest value exist" << endl;
+    }
+
+else{
+
+    cout << "Second largest number is : " << uncmn[k-2] << endl; 
+}
 
     return 0;
 }
